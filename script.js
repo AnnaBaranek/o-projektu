cbc18e18aa0a9b797055b163cde3b64fc17b5c69
Anna Baranek <annaxbaranek@gmail.com>
2024-05-18 20:24:49 +0200

    js text edit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const handleSubmit = (event) => {
     event.preventDefault();
     const jmeno = inputField.value;
     console.log(jmeno);
-    greetContainer.innerHTML =  `<p class="jsText">Ahoj, ${jmeno}! Díky za tvuj zájem. Právě teď Adéla a Anička pilně mě vyvíjejí, abych mohla co nejlépe pomoci ženám s jejich problémy při kojení. Mrkni zatím do sekce <a href="o_projektu.html">o projektu</a>!</p></div>`;
+    greetContainer.innerHTML =  `<p class="jsText">Ahoj, ${jmeno}! Díky za tvůj zájem. Právě teď Adéla a Anička mě pilně vyvíjejí, abych mohla co nejlépe pomoci ženám s jejich problémy při kojení. Mrkni zatím do sekce <a href="o_projektu.html">o projektu</a>!</p></div>`;
 };
 
 form.addEventListener("submit", handleSubmit);
