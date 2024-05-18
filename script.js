c496edfdd4fe85ee5c26fc9b6cac638316e58fe0
Anna Baranek <annaxbaranek@gmail.com>
2024-05-18 20:15:21 +0200

    main page aaded, some css edited

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('#nameForm');
+const inputField = document.getElementById("nameInput");
+const greetContainer = document.getElementById("greetContainer");
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const jmeno = inputField.value;
+    console.log(jmeno);
+    greetContainer.innerHTML =  `<p class="jsText">Ahoj, ${jmeno}! Díky za tvuj zájem. Právě teď Adéla a Anička pilně mě vyvíjejí, abych mohla co nejlépe pomoci ženám s jejich problémy při kojení. Mrkni zatím do sekce <a href="o_projektu.html">o projektu</a>!</p></div>`;
+};
+
+form.addEventListener("submit", handleSubmit);
+
 
