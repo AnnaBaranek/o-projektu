5ace41eddac3489355adcfb20612a980112b8af4
Adéla Přibylová <adelpribyl@gmail.com>
2024-05-19 15:58:53 +0200

    Small correction in text

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const form = document.querySelector('#nameForm');
-const inputField = document.getElementById("nameInput");
-const greetContainer = document.getElementById("greetContainer");
+const inputField = document.getElementById('nameInput');
+const greetContainer = document.getElementById('greetContainer');
 
 const handleSubmit = (event) => {
-    event.preventDefault();
-    const jmeno = inputField.value;
-    console.log(jmeno);
-    greetContainer.innerHTML =  `<p class="jsText">Ahoj, ${jmeno}! Díky za tvůj zájem. Právě teď Adéla a Anička mě pilně vyvíjejí, abych mohla co nejlépe pomoci ženám s jejich problémy při kojení. Mrkni zatím do sekce <a href="o_projektu.html">o projektu</a>!</p></div>`;
+  event.preventDefault();
+  const jmeno = inputField.value;
+  console.log(jmeno);
+  greetContainer.innerHTML = `<p class="jsText">Ahoj, ${jmeno}! Díky za tvůj zájem. Právě teď mě Adéla a Anička pilně vyvíjejí, abych mohl co nejlépe pomoci ženám s jejich problémy při kojení. Mrkni se zatím do sekce <a href="o_projektu.html">o projektu</a>!</p></div>`;
 };
 
-form.addEventListener("submit", handleSubmit);
-
-
+form.addEventListener('submit', handleSubmit);
